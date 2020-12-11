d4db5ccb44f9ebb490fdcf07c65845dab6485d65
arpanaray77 <arpanaray75897@gmail.com>
2020-12-12 01:14:44 +0530

    Adding the Coins in the game

--- core/src/com/app/game/CoinMan.java
+++ core/src/com/app/game/CoinMan.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class CoinMan extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
@@ -15,7 +18,11 @@ public class CoinMan extends ApplicationAdapter {
 	float gravity=0.5f;
 	float velocity=0;
 	int manY=0;
-
+	ArrayList<Integer> coinXs=new ArrayList<>();
+    ArrayList<Integer> coinYs=new ArrayList<>();
+    Texture coin;
+    int coinCount;
+    Random random;
 
 
 	@Override
@@ -29,12 +36,39 @@ public class CoinMan extends ApplicationAdapter {
         man[2]=new Texture("frame3.png");
         man[3]=new Texture("frame4.png");
         manY=Gdx.graphics.getHeight()/2;
+        coin=new Texture("coinSmall.png");
+        random=new Random();
+	}
+
+	public void makeCoin()
+	{
+		float height=350+random.nextFloat()*Gdx.graphics.getHeight();
+		coinYs.add((int)height);
+		coinXs.add(Gdx.graphics.getWidth());
+
 	}
 
 	@Override
 	public void render () {
      batch.begin();
      batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+
+     if(coinCount<100)
+	 {
+	 	coinCount++;
+	 }
+     else
+	 {
+	 	coinCount=0;
+	 	makeCoin();
+	 }
+
+     for (int i=0;i<coinXs.size();i++)
+	 {
+	 	batch.draw(coin,coinXs.get(i),coinYs.get(i));
+	 	coinXs.set(i,coinXs.get(i)-4);
+	 }
+
      if(Gdx.input.justTouched()) {
      	velocity-=10;
 	 }
